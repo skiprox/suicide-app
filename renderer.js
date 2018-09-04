2f9500547e8e98b159dc6be51baaf1b941af5dd6
skiprox <seanmichaelscanlan@gmail.com>
2018-09-04 12:06:57 -0400

    First commit

---
+++ renderer.js
@@ -0,0 +1,12 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+'use strict';
+
+class Renderer {
+	constructor() {
+		console.log('hello world');
+	}
+}
+
+new Renderer();
